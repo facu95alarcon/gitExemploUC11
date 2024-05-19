44371327332d8f5be216a7511b193371661e845e
“Facundo <167827292+facu95alarcon@users.noreply.github.com>
2024-05-19 16:41:05 -0300

    Hola Mundo
    alteração no main.

--- gitHub-Exemplo/src/main/java/com/mycompany/github/exemplo/GitHubExemplo.java
+++ gitHub-Exemplo/src/main/java/com/mycompany/github/exemplo/GitHubExemplo.java
@@ -1,16 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.github.exemplo;
 
-/**
- *
- * @author User
- */
+
 public class GitHubExemplo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola Mundo!!!");
     }
 }
