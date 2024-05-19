19b4b1b0381637d6572230317f75579b8996a2d9
“Facundo <167827292+facu95alarcon@users.noreply.github.com>
2024-05-19 16:32:31 -0300

    Criação do projeto

---
+++ gitHub-Exemplo/src/main/java/com/mycompany/github/exemplo/GitHubExemplo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.github.exemplo;
+
+/**
+ *
+ * @author User
+ */
+public class GitHubExemplo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/gitexemplo/GitExemplo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gitexemplo;
+
+/**
+ *
+ * @author User
+ */
+public class GitExemplo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
